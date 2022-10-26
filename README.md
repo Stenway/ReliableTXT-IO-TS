f710e507ecaf6b1beb3d0cea980c1ca85df61871
Stenway <79376889+Stenway@users.noreply.github.com>
2022-10-26 14:12:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿# ReliableTXT-IO
+
+## About
+
+[ReliableTXT Documentation/Specification](https://www.reliabletxt.com)
+
+## Installation
+
+Using NPM:
+```
+npm install @stenway/reliabletxt-io
+```
+
+## Getting started
+
+```ts
+import { ReliableTxtDocument } from '@stenway/reliabletxt'
+import { ReliableTxtFile } from '@stenway/reliabletxt-io'
+
+let filePath: string = "Test.txt"
+ReliableTxtFile.saveSync(new ReliableTxtDocument("Hello world"), filePath)
+console.log(ReliableTxtFile.loadSync(filePath))
+```
---
+++ dist/reliabletxt-io.d.ts
@@ -0,0 +1,47 @@
+import { ReliableTxtDocument, ReliableTxtEncoding } from '@stenway/reliabletxt';
+export declare abstract class ReliableTxtFile {
+    static getEncodingOrNullSync(filePath: string): ReliableTxtEncoding | null;
+    static getEncodingSync(filePath: string): ReliableTxtEncoding;
+    static loadSync(filePath: string): ReliableTxtDocument;
+    static appendAllTextSync(content: string, filePath: string, createWithEncoding?: ReliableTxtEncoding): void;
+    static appendAllLinesSync(lines: string[], filePath: string, createWithEncoding?: ReliableTxtEncoding): void;
+    static readAllTextSync(filePath: string): string;
+    static readAllLinesSync(filePath: string): string[];
+    static saveSync(document: ReliableTxtDocument, filePath: string): void;
+    static writeAllTextSync(content: string, filePath: string, encoding?: ReliableTxtEncoding): void;
+    static writeAllLinesSync(lines: string[], filePath: string, encoding?: ReliableTxtEncoding): void;
+}
+export declare class SyncReliableTxtStreamReader {
+    private handle;
+    private position;
+    private buffer;
+    private _encoding;
+    private rest;
+    get encoding(): ReliableTxtEncoding;
+    get isClosed(): boolean;
+    constructor(filePath: string, chunkSize?: number);
+    readLine(): string | null;
+    close(): void;
+}
+export declare class SyncReliableTxtStreamWriter {
+    private handle;
+    readonly encoding: ReliableTxtEncoding;
+    private isEmpty;
+    get isClosed(): boolean;
+    constructor(filePath: string, createWithEncoding?: ReliableTxtEncoding, append?: boolean);
+    write(text: string): void;
+    writeLine(line: string): void;
+    writeLines(lines: string[]): void;
+    private validateIsOpen;
+    close(): void;
+}
+export declare class ReverseLineIterator {
+    private handle;
+    private index;
+    private encoding;
+    private buffer;
+    constructor(filePath: string, encoding: ReliableTxtEncoding);
+    getLine(): string;
+    getPosition(): number;
+    close(): void;
+}
